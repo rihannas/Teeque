cd9f92dd3c9fea30e23a1ce1a9de033b70cd466f
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2024-07-27 05:15:21 +0300

    update models

--- teequeapp/models.py
+++ teequeapp/models.py
@@ -1,9 +1,33 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser, BaseUserManager, Group, Permission
-from phonenumber_field.modelfields import PhoneNumberField
-from django_countries.fields import CountryField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+from django_countries.fields import CountryField
+
+from phonenumber_field.modelfields import PhoneNumberField
+from decimal import Decimal
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)  
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 
@@ -16,7 +40,7 @@ class CustomUser(AbstractUser):
     last_login = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'about', 'phonenumber']
+    REQUIRED_FIELDS = ['username', 'about', 'phonenumber', 'password']
 
 
     # Add unique related_name attributes to avoid clashes
@@ -37,14 +61,14 @@ class CustomUser(AbstractUser):
 
 
 class Seller(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='seller')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='seller')
     skills = models.TextField(blank=True, null=True)
     portfolio = models.TextField(blank=True, null=True)
     average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
     number_of_reviews = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username}'
 
     def save(self, *args, **kwargs):
         # Add the user to the Seller group
@@ -53,11 +77,11 @@ class Seller(models.Model):
         super().save(*args, **kwargs)
 
 class Buyer(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='buyer')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='buyer')
     favorite_services = models.ManyToManyField('Service', related_name='favorited_by')
 
     def __str__(self):
-        return self.user.username
+        return f'{self.user.username}'
 
     def save(self, *args, **kwargs):
         # Add the user to the Seller group
@@ -74,30 +98,49 @@ class Category(models.Model):
 
 
 class Service(models.Model):
-    title = models.CharField(max_length=225)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=6, validators=[MinValueValidator(0.00)])
+    title = models.CharField(max_length=225, blank=False)
+    category_id = models.ForeignKey(Category, on_delete=models.PROTECT, blank=False)
+    description = models.TextField(blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=6, validators=[MinValueValidator(Decimal(0.00))], blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-    tags = models.ManyToManyField('Tag', related_name='services')
+    seller_id = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', related_name='services', blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Order(models.Model):
-    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    buyer_id = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    seller_id = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    #unit price
 
     #TODO: 
     # Change the on_delete situation to be PROTECTED if an order is active.
     def __str__(self):
         return f"Order #{self.id}"
+    
+class OrderItem(models.Model):
+    ORDER_STATUS_PENDING = 'P'
+    ORDER_STATUS_COMPLETE = 'C'
+    ORDER_STATUS_DELIEVERED = 'D'
+    ORDER_STATUS_REVISION = 'R'
+    ORDER_STATUS_CHOICES = [
+        (ORDER_STATUS_PENDING, 'Pending'),
+        (ORDER_STATUS_COMPLETE, 'Completed'),
+        (ORDER_STATUS_DELIEVERED, 'Delievered'),
+        (ORDER_STATUS_REVISION, 'Revision')
+    ]
 
+    order_id = models.ForeignKey(Order, on_delete=models.PROTECT)
+    service_id = models.ForeignKey(Service, on_delete=models.PROTECT, related_name='orderitems')
+    order_status = models.CharField(
+        max_length=1, choices=ORDER_STATUS_CHOICES, default=ORDER_STATUS_PENDING)
+
+    def __str__(self) -> str:
+        return f"{self.service}"
 
 class Tag(models.Model):
     tag = models.CharField(max_length=50, unique=True)
@@ -107,11 +150,11 @@ class Tag(models.Model):
 
 
 class Rating(models.Model):
-    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='reviews')
-    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    service_id = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='reviews')
+    buyer_id = models.ForeignKey(Buyer, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     comment = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"Rating for {self.service.title} by {self.buyer.username}"
@@ -119,3 +162,8 @@ class Rating(models.Model):
 
 class Transaction(models.Model):
     pass
+
+
+#Servicevariations : basic, mid, premium
+
+#Paymentsession instead of cart
